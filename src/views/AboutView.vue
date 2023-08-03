7fafb3394ab164aa5687fc8924f2b73ea0e19e4f
yancheesetofer <yan.christofer@ui.ac.id>
2023-08-03 15:29:57 +0700

    Modify logo and About section.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,11 +8,12 @@
     <p
       class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300"
     >
-      Passionate with software engineering and information security. 
-      I have built recognized projects using Springboot(Java), ReactJS, Django(Python),
-      Flutter(Dart), and here I am presenting you a webapp using VueJS.
+      Fueling my passion for software engineering and information security, 
+      I've crafted remarkable projects using Springboot (Java), ReactJS, Django (Python), Flutter (Dart),
+      and here I am presenting you a webapp using VueJS.
     </p>
-    <p class="text-center text-blue-600"></p>
+    <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto text-yellow-500">Currently, I'm pursuing Computer Science degree at the University of Indonesia.🚀</p>
+    <p class="text-center text-blue-600"> {toggle at the top right corner}</p>
   </div>
 </template>
 <script setup></script>
